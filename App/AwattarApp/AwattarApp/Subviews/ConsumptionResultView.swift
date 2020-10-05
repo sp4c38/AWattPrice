1535a91ec1a15db6bc22222c5078bdfd214fca05
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-10-05 20:53:14 +0200

    Consumption Result View improvments

--- App/AwattarApp/AwattarApp/Subviews/ConsumptionResultView.swift
+++ App/AwattarApp/AwattarApp/Subviews/ConsumptionResultView.swift
@@ -12,32 +12,44 @@ struct ConsumptionResultView: View {
     
     var cheapestHourCalculator: CheapestHourCalculator
     var dateFormatter = DateFormatter()
-    @State var cheapestTimeIntervalSince1970: TimeInterval = 0
+    @State var startOfCheapestHours: TimeInterval = 0
+    @State var endOfCheapestHours: TimeInterval = 0
     
     init(cheapestHourCalculator: CheapestHourCalculator) {
         self.cheapestHourCalculator = cheapestHourCalculator
   
         dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .medium
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
     }
     
     var body: some View {
         VStack {
             if cheapestHourCalculator.cheapestHoursForUsage != nil {
-                HStack {
-                    Text(dateFormatter.string(from: Date(timeIntervalSince1970: cheapestTimeIntervalSince1970)))
+                HStack(spacing: 20) {
+                    Text(dateFormatter.string(from: Date(timeIntervalSince1970: startOfCheapestHours)))
+                        .bold()
                         .onAppear {
-                            cheapestTimeIntervalSince1970 =  TimeInterval(cheapestHourCalculator.cheapestHoursForUsage!.associatedPricePoints[cheapestHourCalculator.cheapestHoursForUsage!.associatedPricePoints.count - 1].startTimestamp + (cheapestHourCalculator.cheapestHoursForUsage!.differenceIsBefore ? cheapestHourCalculator.cheapestHoursForUsage!.minuteDifferenceInSeconds : 0))
+                            startOfCheapestHours =  TimeInterval((cheapestHourCalculator.cheapestHoursForUsage!.associatedPricePoints[0].startTimestamp / 1000) + (cheapestHourCalculator.cheapestHoursForUsage!.differenceIsBefore ? cheapestHourCalculator.cheapestHoursForUsage!.minuteDifferenceInSeconds : 0))
                         }
                     
                     Text("until")
                     
-//                    Text(dateFormatter.string(from: Date(timeIntervalSince1970:  TimeInterval(hour.endTimestamp / 1000))))
+                    Text(dateFormatter.string(from: Date(timeIntervalSince1970: endOfCheapestHours)))
+                        .bold()
+                        .onAppear {
+                            endOfCheapestHours =  TimeInterval((cheapestHourCalculator.cheapestHoursForUsage!.associatedPricePoints[cheapestHourCalculator.cheapestHoursForUsage!.associatedPricePoints.count - 1].endTimestamp / 1000) + (cheapestHourCalculator.cheapestHoursForUsage!.differenceIsBefore ? cheapestHourCalculator.cheapestHoursForUsage!.minuteDifferenceInSeconds : 0))
+                        }
                 }
+                .font(.title)
+                
+                Spacer()
                 
                 ConsumptionClockView(cheapestHour: cheapestHourCalculator.cheapestHoursForUsage!)
                     .frame(maxHeight: 200)
+                
+                Spacer()
+                Spacer()
             } else {
                 ProgressView()
                     .progressViewStyle(CircularProgressViewStyle())

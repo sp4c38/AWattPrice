359f53d3253413376dc2dbac644722b5b7fdeee0
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-10-17 22:46:49 +0200

    Updated last Splash Screen
    Some UI improvments to the Splash Screen:
     - Will now display a green checkmark which animates (animation with SwiftUI)

--- App/AwattarApp/AwattarApp/SplashScreens/SplashScreenFinishView.swift
+++ App/AwattarApp/AwattarApp/SplashScreens/SplashScreenFinishView.swift
@@ -7,31 +7,144 @@
 
 import SwiftUI
 
-struct SplashScreenFinishView: View {
-    var body: some View {
-        VStack(spacing: 0) {
-            Spacer()
+struct CheckmarkView: View {
+    struct CheckmarkFirstLine: Shape {
+        var startPoint: CGPoint
+        var endPoint: CGPoint
+        let lineWidth: CGFloat
+        
+        var animatableData: AnimatablePair<CGFloat, CGFloat> {
+            get {
+                AnimatablePair(endPoint.x, endPoint.y)
+            }
+
+            set {
+                self.endPoint.x = newValue.first
+                self.endPoint.y = newValue.second
+            }
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.move(to: CGPoint(x: startPoint.x, y: startPoint.y))
+            path.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+            
+            path = path.strokedPath(StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+            return path
+        }
+    }
+    
+    struct CheckmarkEndLine: Shape {
+        var startPoint: CGPoint
+        var endPoint: CGPoint
+        let lineWidth: CGFloat
+        
+        var animatableData: AnimatablePair<CGFloat, CGFloat> {
+            get {
+                AnimatablePair(endPoint.x, endPoint.y)
+            }
+
+            set {
+                self.endPoint.x = newValue.first
+                self.endPoint.y = newValue.second
+            }
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            if endPoint.x != startPoint.x && endPoint.y != startPoint.y {
+                path.move(to: CGPoint(x: startPoint.x, y: startPoint.y))
+                path.addLine(to: CGPoint(x: endPoint.x, y: endPoint.y))
+                
+                path = path.strokedPath(StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+            }
+            
+            return path
+        }
+    }
+    
+    @State var trimAmount: CGFloat = 0.01
+    @State var firstLineStartPoint = CGPoint(x: 0, y: 0)
+    @State var firstLineEndPoint = CGPoint(x: 0, y: 0)
+    @State var secondLineStartPoint = CGPoint(x: 0, y: 0)
+    @State var secondLineEndPoint = CGPoint(x: 0, y: 0)
+    
+    func makeView(_ geometry: GeometryProxy) -> some View {
+        let width = geometry.size.width
+        let height = geometry.size.height
+        
+        let checkmarkWidth = width / 3
+        let checkmarkStartWidth = (width - checkmarkWidth) / 2
+        let checkmarkStartHeight: CGFloat = (height / 3) - (checkmarkWidth / 2)
+        
+        let lineWidth: CGFloat = checkmarkWidth / 17
+
+        return ZStack {
+            ZStack {
+                CheckmarkFirstLine(startPoint: firstLineStartPoint, endPoint: firstLineEndPoint, lineWidth: lineWidth)
+                
+                CheckmarkEndLine(startPoint: secondLineStartPoint, endPoint: secondLineEndPoint, lineWidth: lineWidth)
+                
+                Circle()
+                    .trim(from: 0.0, to: trimAmount)
+                    .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                    .frame(width: checkmarkWidth, height: checkmarkWidth)
+                    .position(x: width / 2, y: checkmarkStartHeight + (checkmarkWidth / 2))
+            }
+            .foregroundColor(Color.green)
             
             VStack(spacing: 30) {
-                Image(systemName: "checkmark.circle")
-                    .foregroundColor(Color.green)
-                    .font(.system(size: 100, weight: .regular))
-                
-                Text("Setup finished")
-                    .font(.system(size: 30, weight: .black))
-                    .padding(.bottom, 5)
-                
-                Text("You will be able to change your\n settings again later.")
-                    .multilineTextAlignment(.center)
+               Text("Setup finished")
+                   .font(.system(size: 30, weight: .black))
+                   .padding(.bottom, 5)
+
+               Text("You still can change your\n settings later.")
+                   .multilineTextAlignment(.center)
+           }
+           .position(x: width / 2, y: checkmarkStartHeight + 2 * checkmarkWidth)
+        }
+        .onAppear {
+            firstLineStartPoint = CGPoint(x: 0.294 * checkmarkWidth + checkmarkStartWidth, y: 0.530 * checkmarkWidth + checkmarkStartHeight)
+            firstLineEndPoint = firstLineStartPoint
+            
+            secondLineStartPoint = CGPoint(x: 0.437 * checkmarkWidth + checkmarkStartWidth, y: 0.710 * checkmarkWidth + checkmarkStartHeight)
+            secondLineEndPoint = secondLineStartPoint
+            
+            withAnimation(Animation.easeOut(duration: 2.7)) {
+                trimAmount = 1
             }
             
-            Spacer()
-            Spacer()
-            
-            Button(action: {}) {
-                Text("Finish")
+            withAnimation(Animation.easeIn(duration: 0.9)) {
+                firstLineEndPoint = secondLineStartPoint
             }
-            .buttonStyle(ContinueButtonStyle())
+            
+            withAnimation(Animation.easeOut(duration: 1.8).delay(0.9)) {
+                secondLineEndPoint = CGPoint(x: 0.695 * checkmarkWidth + checkmarkStartWidth, y: 0.308 * checkmarkWidth + checkmarkStartHeight)
+            }
+        }
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            makeView(geometry)
+        }
+    }
+}
+
+struct SplashScreenFinishView: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Spacer()
+
+                Button(action: {}) {
+                    Text("Finish")
+                }
+                .buttonStyle(ContinueButtonStyle())
+            }
+            
+            CheckmarkView()
         }
         .padding()
     }

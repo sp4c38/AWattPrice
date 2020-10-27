c488de1eb68fe3c9f16a78cdc489409942fdb78c
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-10-27 20:54:39 +0100

    Updates to Help & Suggestions View
    - added dark mode support
    - users now can send in suggestions
    - improvments in performance and code readability

--- App/AWattPrice/AWattPrice/Supporting Views/HelpMailView.swift
+++ App/AWattPrice/AWattPrice/Supporting Views/HelpMailView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 import MessageUI
 
-struct HelpMailView: UIViewControllerRepresentable {
+class MailContent {
+    var recipientEmails: String
+    var subject: String
+    var encodedSubject: String
+    var body: String
+    var encodedBody: String
+    
+    init(recipientEmails: String, subject: String, encodedSubject: String, body: String, encodedBody: String) {
+        self.recipientEmails = recipientEmails
+        self.subject = subject
+        self.encodedSubject = encodedSubject
+        self.body = body
+        self.encodedBody = encodedBody
+    }
+}
+
+class HelpMailContent: MailContent {
+    init() {
+        let recipientEmails = "help-awattprice@space8.me"
+        let subject = "AWattPrice Help"
+        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    
+        var messageBody = ""
+        
+        if let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
+            messageBody += "\n\nApp version number: "
+            
+            if let currentAppName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+                messageBody += "\(currentAppName) "
+            }
+            messageBody += "\(currentVersion)"
+        }
+        
+        if let currentBuild = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String {
+            messageBody += " (\(currentBuild))"
+        }
+        
+        let body = messageBody
+        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+
+        super.init(
+            recipientEmails: recipientEmails,
+            subject: subject,
+            encodedSubject: encodedSubject,
+            body: body,
+            encodedBody: encodedBody)
+    }
+}
+
+class SuggestionMailContent: MailContent {
+    init() {
+        let recipientEmails = "suggestion-awattprice@space8.me"
+        let subject = "AWattPrice Suggestion"
+        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+        
+        let body = ""
+        let encodedBody = ""
+
+        super.init(
+            recipientEmails: recipientEmails,
+            subject: subject,
+            encodedSubject: encodedSubject,
+            body: body,
+            encodedBody: encodedBody)
+    }
+}
+
+struct MailView: UIViewControllerRepresentable {
     @Environment(\.presentationMode) var presentationMode
+    
+    let mailContent: MailContent
     
     class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
         @Binding var presentationMode: PresentationMode
@@ -31,46 +100,12 @@ struct HelpMailView: UIViewControllerRepresentable {
         
     }
     
-    struct HelpMail {
-        let recipientEmails: String
-        let subject: String
-        let encodedSubject: String
-        var body: String = ""
-        var encodedBody: String = ""
-        
-        init() {
-            self.recipientEmails = "support@space8.me"
-            self.subject = "AWattPrice Help"
-            self.encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        
-            var messageBody = ""
-            
-            if let currentAppName = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
-                messageBody += "\n\nApp name: \(currentAppName)\n"
-            }
-            
-            if let currentVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
-                messageBody += "App version number: \(currentVersion)"
-            }
-            
-            if let currentBuild = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String {
-                messageBody += " (\(currentBuild))"
-            }
-            
-            self.body = messageBody
-            self.encodedBody = self.body.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-            print(self.encodedBody)
-        }
-    }
-
-    var helpMail = HelpMail()
-    
     func makeUIViewController(context: Context) -> MFMailComposeViewController {
         let mailComposerViewController = MFMailComposeViewController()
         mailComposerViewController.mailComposeDelegate = context.coordinator
-        mailComposerViewController.setSubject(helpMail.subject)
-        mailComposerViewController.setToRecipients([helpMail.recipientEmails])
-        mailComposerViewController.setMessageBody(helpMail.body, isHTML: false)
+        mailComposerViewController.setSubject(mailContent.subject)
+        mailComposerViewController.setToRecipients([mailContent.recipientEmails])
+        mailComposerViewController.setMessageBody(mailContent.body, isHTML: false)
         
         return mailComposerViewController
     }
@@ -84,11 +119,11 @@ struct HelpMailView: UIViewControllerRepresentable {
     }
     
     func getAlternativeMailApp() -> URL? {
-        let gmailUrl = URL(string: "googlegmail://co?to=\(helpMail.recipientEmails)&subject=\(helpMail.encodedSubject)&body=\(helpMail.encodedBody)")
-        let outlookUrl = URL(string: "ms-outlook://compose?to=\(helpMail.recipientEmails)&subject=\(helpMail.encodedSubject)")
-        let yahooMail = URL(string: "ymail://mail/compose?to=\(helpMail.recipientEmails)&subject=\(helpMail.encodedSubject)&body=\(helpMail.encodedBody)")
-        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(helpMail.recipientEmails)&subject=\(helpMail.encodedSubject)&body=\(helpMail.encodedBody)")
-        let defaultUrl = URL(string: "mailto:\(helpMail.recipientEmails)?subject=\(helpMail.encodedSubject)&body=\(helpMail.encodedBody)")
+        let gmailUrl = URL(string: "googlegmail://co?to=\(mailContent.recipientEmails)&subject=\(mailContent.encodedSubject)&body=\(mailContent.encodedBody)")
+        let outlookUrl = URL(string: "ms-outlook://compose?to=\(mailContent.recipientEmails)&subject=\(mailContent.encodedSubject)")
+        let yahooMail = URL(string: "ymail://mail/compose?to=\(mailContent.recipientEmails)&subject=\(mailContent.encodedSubject)&body=\(mailContent.encodedBody)")
+        let sparkUrl = URL(string: "readdle-spark://compose?recipient=\(mailContent.recipientEmails)&subject=\(mailContent.encodedSubject)&body=\(mailContent.encodedBody)")
+        let defaultUrl = URL(string: "mailto:\(mailContent.recipientEmails)?subject=\(mailContent.encodedSubject)&body=\(mailContent.encodedBody)")
         
         if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
             return gmailUrl

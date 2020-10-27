8a03946541d687ca375a87288a5e251c80303e49
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-10-27 20:54:39 +0100

    Updates to Help & Suggestions View

---
+++ App/AWattPrice/AWattPrice/Supporting Views/HelpAndSuggestionView.swift
@@ -0,0 +1,171 @@
+//
+//  HelpView.swift
+//  AWattPrice
+//
+//  Created by Léon Becker on 27.10.20.
+//
+
+import MessageUI
+import SwiftUI
+
+struct HelpSuggestionButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(Color.white)
+            .frame(maxWidth: .infinity)
+            .padding([.top, .bottom], 16)
+            .padding([.leading, .trailing], 5)
+            .background(Color.blue)
+            .cornerRadius(10)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 5)
+            )
+        
+    }
+}
+
+struct HelpView: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    @State var isShowingMailView = false
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Text("help")
+                .bold()
+                .font(.title)
+            
+            Button(action: {
+                if MFMailComposeViewController.canSendMail() {
+                    self.isShowingMailView.toggle()
+                } else {
+                    if let alternativeUrl = MailView(mailContent: HelpMailContent()).getAlternativeMailApp() {
+                        UIApplication.shared.open(alternativeUrl)
+                    }
+                }
+            }) {
+                HStack(spacing: 10) {
+                    Image(systemName: "envelope")
+                        .font(.system(size: 20, weight: .semibold))
+                    
+                    Text("helpByEmail")
+                        .font(.title3)
+                        .bold()
+                }
+            }
+            .buttonStyle(HelpSuggestionButtonStyle())
+        }
+        .padding()
+        .background(colorScheme == .light ? Color(hue: 0.6667, saturation: 0.0340, brightness: 0.8985) : Color(hue: 0.6667, saturation: 0.0340, brightness: 0.1015))
+        .cornerRadius(20)
+        .padding()
+        .sheet(isPresented: $isShowingMailView) {
+            MailView(mailContent: HelpMailContent())
+                .edgesIgnoringSafeArea(.bottom)
+        }
+    }
+}
+
+struct SuggestionView: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    @State var isShowingMailView = false
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Text("suggestion")
+                .bold()
+                .font(.title)
+            
+            Button(action: {
+                if MFMailComposeViewController.canSendMail() {
+                    self.isShowingMailView.toggle()
+                } else {
+                    if let alternativeUrl = MailView(mailContent: SuggestionMailContent()).getAlternativeMailApp() {
+                        UIApplication.shared.open(alternativeUrl)
+                    }
+                }
+            }) {
+                HStack(spacing: 10) {
+                    Image("suggestion")
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: 25, height: 25)
+                    
+                    Text("suggestionByEmail")
+                        .font(.title3)
+                        .bold()
+                }
+            }
+            .buttonStyle(HelpSuggestionButtonStyle())
+        }
+        .padding()
+        .background(colorScheme == .light ? Color(hue: 0.6667, saturation: 0.0340, brightness: 0.8985) : Color(hue: 0.6667, saturation: 0.0340, brightness: 0.1015))
+        .cornerRadius(20)
+        .padding()
+        .sheet(isPresented: $isShowingMailView) {
+            MailView(mailContent: SuggestionMailContent())
+                .edgesIgnoringSafeArea(.bottom)
+        }
+    }
+}
+
+struct HelpAndSuggestionView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .center)) {
+            if colorScheme == .light {
+                Color(hue: 0.6667, saturation: 0.0202, brightness: 0.9686)
+                    .ignoresSafeArea()
+            } else {
+                Color.black
+                    .ignoresSafeArea()
+            }
+
+            VStack {
+                HelpView()
+                SuggestionView()
+            }
+        }
+        .navigationTitle("helpAndSuggestions")
+    }
+}
+
+struct GetHelpView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+//        Section {
+            NavigationLink(destination: HelpAndSuggestionView()) {
+                HStack {
+                    Image(systemName: "questionmark.circle")
+                        .font(.title2)
+                    
+                    Text("helpAndSuggestions")
+                        .font(.subheadline)
+                }
+                .foregroundColor(colorScheme == .light ? Color.black : Color.white)
+            }
+//        }
+    }
+}
+
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+//        NavigationView {
+//            List {
+//                HelpView
+//            }
+//            .listStyle(InsetGroupedListStyle())
+//            .navigationTitle("Settings")
+//        }
+//        .preferredColorScheme(.light)
+        
+        NavigationView {
+            HelpAndSuggestionView()
+        }
+        .preferredColorScheme(.light)
+    }
+}

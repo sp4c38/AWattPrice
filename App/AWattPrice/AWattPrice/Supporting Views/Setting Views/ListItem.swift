22a8fd917a7bbc3771628cd1a30fc8e544372090
sp4c38 <36799235+sp4c38@users.noreply.github.com>
2020-10-31 16:29:36 +0100

    Added custom list and improvments
    Added a custom list.
    Improved the awattar tariff selection.

---
+++ App/AWattPrice/AWattPrice/Supporting Views/Setting Views/ListItem.swift
@@ -0,0 +1,20 @@
+//
+//  ListItem.swift
+//  AWattPrice
+//
+//  Created by Léon Becker on 31.10.20.
+//
+
+import SwiftUI
+
+struct ListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ListItem()
+    }
+}

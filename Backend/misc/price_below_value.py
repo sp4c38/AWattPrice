9e09805724e6f5868ff2f1a6f7f33312feeb07e0
Léon Becker <lb@alien8.de>
2021-01-09 01:45:48 +0100

    Completed price_below_value.py
    Will now give you a nice output to the console when the longest time range starts and ends, in which prices drop below a certain value.

--- Backend/misc/price_below_value.py
+++ Backend/misc/price_below_value.py
@@ -19,9 +19,12 @@ __author__ = "Léon Becker (sp4c38) <lb@space8.me>"
 __copyright__ = "Léon Becker"
 __license__ = "mit"
 
+import arrow
 import json
 
 from box import Box
+from datetime import datetime
+from dateutil.tz import tzstr
 from pathlib import Path
 from typing import Optional
 
@@ -80,21 +83,55 @@ def recursiv_grouping(current_item_index: Optional[int], items_list: list, belon
 
     return result_list
 
-def get_grouped_list(filtered_list: list, all_data) -> list:
+def get_grouped_list(filtered_list: list, all_data: list) -> list:
     # Form a list out of the filtered list which groups together sequential items
     # Example input:
     # [(0, 5), (3, 12), (7, -2), (8, -3), (9, -7), (11, 14)] ... where the first item of a tuple is the index in the original list
     # Associated example output:
     # [(7, -2), (8, -3), (9, -7)]
 
-    filtered_list = [(5, 5), (6, 8), (7, 10), (8, 2), (9, 200), (10, 13), (11, 19), (14, 100), (15, 200)]
     if len(filtered_list) >= 2:
         if filtered_list[0][0] + 1 == filtered_list[1][0]: 
-            print("final")
-            print(recursiv_grouping(None, filtered_list, True))
+            return recursiv_grouping(None, filtered_list, True)
         else:
-            print("final")
-            print(recursiv_grouping(None, filtered_list, False))
+            return recursiv_grouping(None, filtered_list, False)
+    else:
+        return [filtered_list[0]]
+        
+def get_longest_time_frame(grouped_list: list, all_data: list):
+    longest_time_frame_index = (None, None) # Index in grouped_list, number of the items in the item represented by the index
+    current_index = 0
+    for grouped_items in grouped_list:
+        if longest_time_frame_index[0] == None or longest_time_frame_index[1] == None:
+            if type(grouped_items[0]) is int:
+                longest_time_frame_index = (current_index, 1)
+            else:
+                longest_time_frame_index = (current_index, len(grouped_items))
+        else:
+            amount_of_items = 0
+            if type(grouped_items[0]) is int:
+                amount_of_items = 1
+            else:
+                amount_of_items = len(grouped_items)
+                
+            if amount_of_items > longest_time_frame_index[1]:
+                longest_time_frame_index = (current_index, amount_of_items)
+                
+        current_index += 1
+        
+    return longest_time_frame_index[0]
+
+def get_start_end_string(longest_time_range: list) -> (str, str):
+    raw_items = []
+    start = arrow.get(min(longest_time_range)[1].start_timestamp)
+    end = arrow.get(max(longest_time_range)[1].end_timestamp)
+    
+    timezone = timezone = tzstr("CET-1CEST,M3.5.0/2,M10.5.0/3").tzname(datetime.fromtimestamp(start.timestamp))
+    format_string = "YYYY-MM-DD, HH:mm:ss"
+    start_string = start.to(timezone).format(format_string)
+    end_string = end.to(timezone).format(format_string)
+
+    return start_string, end_string
 
 def main():
     price_data_path = Path("~/awattprice/data/awattar-data-de.json").expanduser()
@@ -106,10 +143,17 @@ def main():
         price_point.marketprice = round(price_point.marketprice, 2)
         price_data.append(price_point)
 
-    on_below_value = 5#int(input("This script will find the longest time range in which price drop on or below (int and in cents): "))
+    on_below_value = int(input("This script will find the longest time range in which price drop on or below (int and in cents): "))
     
     below_list = get_below_list(price_data, on_below_value)
-    grouped_list = get_grouped_list(below_list, price_data)
+    if below_list:
+        grouped_list = get_grouped_list(below_list, price_data)
+        longest_time_range_index = get_longest_time_frame(grouped_list, price_data) # Index of which item in grouped_list has the most elements
+        start_date_string, end_date_string = get_start_end_string(grouped_list[longest_time_range_index])
+    
+        print(f"The longest time range in which prices fall below {on_below_value}ct is from {start_date_string} to {end_date_string} (times in CET / CEST).")
+    else:
+        print(f"No results found as there are no price points that drop below {on_below_value}.")
 
 if __name__ == '__main__':
     main()

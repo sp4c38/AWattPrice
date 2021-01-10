4ef117f9586abdda8ba23304d829f972da75fa30
Frank Becker <fb@alien8.de>
2021-01-10 23:43:02 +0100

    apns: Convert validate_token to regular func
    There is no point in making the token validation async.
    
    The await for the api.send_token request better fits
    in the FastAPI part.

--- Backend/src/awattprice/apns.py
+++ Backend/src/awattprice/apns.py
@@ -31,7 +31,7 @@ async def write_token(request_data, db_manager):
     await db_manager.release_lock()
 
 
-async def validate_token(request: Request) -> Optional[Dict]:
+def validate_token(request: Request) -> Optional[Dict]:
     """Checks if the backend can successfully read and decode the APNs token configuration
     sent from a client.
 
@@ -41,8 +41,7 @@ async def validate_token(request: Request) -> Optional[Dict]:
     is meant.
     """
 
-    request_body = await request.body()
-    decoded_body = request_body.decode("utf-8")
+    decoded_body = request.decode("utf-8")
 
     try:
         body_json = json.loads(decoded_body)

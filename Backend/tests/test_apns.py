26ceea2ff46589e136ca21bf1bd97f6b4227dcf1
Frank Becker <fb@alien8.de>
2021-01-16 23:49:39 +0100

    backend: Add Unit test for apns.validate_token
    Run by
    
    	python setup.py test

---
+++ Backend/tests/test_apns.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from awattprice.apns import validate_token
+
+__author__ = "Frank Becker"
+__copyright__ = "Frank Becker"
+__license__ = "mit"
+
+
+
+def test_validate_token():
+    data = bytes('{"apnsDeviceToken": "ALovelyApnsToken", "regionIdentifier": 0, "vatSelection": 1, '
+                 '"notificationConfig": {"priceBelowValueNotification": '
+                 '{"active": true, "belowValue": 20}}}'.encode())
+    assert validate_token(data) == {
+        'token': 'ALovelyApnsToken',
+        'region_identifier': 0,
+        'vat_selection': 1,
+        'config': {
+            'price_below_value_notification': {
+                'active': True,
+                'below_value': 20.0
+            }
+        }
+    }

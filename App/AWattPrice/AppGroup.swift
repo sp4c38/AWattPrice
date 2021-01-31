7a8f48a07d380778a2d39e7c76fc8b024df0f14b
Léon Becker <lb@alien8.de>
2021-01-31 09:30:35 +0100

    Added dev and production AppGroup environment

---
+++ App/AWattPrice/AppGroup.swift
@@ -0,0 +1,12 @@
+//
+//  AppGroup.swift
+//  AWattPriceWidgetExtension
+//
+//  Created by Léon Becker on 31.01.21.
+//
+
+import Foundation
+
+enum AppGroup: String {
+    case awattprice = "group.me.space8.AWattPrice"
+}

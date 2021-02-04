582a48dc6db3912037fd6d3f4faa8de17a73c1d4
Léon Becker <lb@alien8.de>
2021-02-04 09:10:24 +0100

    Added new basic UI Tests

--- App/AWattPrice/AWattPriceUITests/AWattPriceUITests.swift
+++ App/AWattPrice/AWattPriceUITests/AWattPriceUITests.swift
@@ -7,45 +7,76 @@
 
 import XCTest
 
+class AWattPriceTestManager {
+    let app: XCUIApplication
+    
+    var staticTexts: XCUIElementQuery?
+    var buttons: XCUIElementQuery?
+    
+    init(_ app: XCUIApplication) {
+        self.app = app
+    }
+    
+    func getStaticTexts() {
+        staticTexts = app.staticTexts
+    }
+    
+    func getButtons() {
+        buttons = app.buttons
+    }
+}
+
+extension AWattPriceTestManager {
+    func testHelpView() {
+        guard let texts = self.staticTexts else { return }
+        let goToHelpButton = texts["Help & Suggestions"]
+        XCTAssertTrue(goToHelpButton.exists)
+        goToHelpButton.tap()
+    }
+}
+
 class AWattPriceUITests: XCTestCase {
-
+    let manager = AWattPriceTestManager(XCUIApplication())
+    
+    override func setUp() {
+        manager.app.launch()
+        manager.getStaticTexts()
+        manager.getButtons()
+    }
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testSettings() throws {
-        let app = XCUIApplication()
-        app.launch()
+    
+    func testSettings() {
+        guard let staticTexts = manager.staticTexts else { return }
         
-        let appStaticText = app.staticTexts
+        let app = manager.app
         
-        let settingsTab = appStaticText["Settings"]
+        let settingsTab = staticTexts["Settings"]
         XCTAssertTrue(settingsTab.exists)
         settingsTab.tap()
-        
+
         let regionGermany = app.buttons["🇩🇪 Germany"]
         let regionAustria = app.buttons["🇦🇹 Austria"]
         XCTAssertTrue(regionGermany.exists)
         XCTAssertTrue(regionAustria.exists)
         regionAustria.tap()
         regionGermany.tap()
+        
+        manager.testHelpView()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }

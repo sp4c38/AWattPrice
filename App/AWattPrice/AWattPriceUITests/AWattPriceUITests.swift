81fd6e3bc1a84e42e14bd9d7c3e798b3687108e5
Léon Becker <lb@alien8.de>
2021-02-04 08:51:37 +0100

    File updates

--- App/AWattPrice/AWattPriceUITests/AWattPriceUITests.swift
+++ App/AWattPrice/AWattPriceUITests/AWattPriceUITests.swift
@@ -2,35 +2,42 @@
 //  AWattPriceUITests.swift
 //  AWattPriceUITests
 //
-//  Created by Léon Becker on 06.09.20.
+//  Created by Léon Becker on 02.02.21.
 //
 
 import XCTest
 
-class AWattPriceAppUITests: XCTestCase {
+class AWattPriceUITests: XCTestCase {
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        /*
-          In UI tests it’s important to set the initial state - such as interface orientation -
-          required for your tests before they run. The setUp method is a good place to do this.
-         */
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSettings() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-                        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let appStaticText = app.staticTexts
+        
+        let settingsTab = appStaticText["Settings"]
+        XCTAssertTrue(settingsTab.exists)
+        settingsTab.tap()
+        
+        let regionGermany = app.buttons["🇩🇪 Germany"]
+        let regionAustria = app.buttons["🇦🇹 Austria"]
+        XCTAssertTrue(regionGermany.exists)
+        XCTAssertTrue(regionAustria.exists)
+        regionAustria.tap()
+        regionGermany.tap()
     }
 
     func testLaunchPerformance() throws {

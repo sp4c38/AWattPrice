af8076364ff613a2aa65768cc0277f46c61848c6
Léon Becker <lb@alien8.de>
2021-02-08 10:54:57 +0100

    Renamed to rotationTime

--- App/AWattPrice/AWattPriceWidget/Price Widget/Timeline.swift
+++ App/AWattPrice/AWattPriceWidget/Price Widget/Timeline.swift
@@ -8,54 +8,6 @@
 import Network
 import WidgetKit
 
-func getNewEnergyData() {
-    
-}
-
-func getTodayNoonDate(fromTimeZone timeZone: String) -> Date {
-    let isoDateFormatter = DateFormatter()
-    isoDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-    isoDateFormatter.timeZone = TimeZone(identifier: "Europe/Berlin")
-    let nowBerlinISO = isoDateFormatter.string(from: Date())
-    
-    let noonTimeDate = nowBerlinISO.prefix(11)
-    let noonTimeTime = "12:00:00"
-    let noonTimeTimezone = nowBerlinISO.suffix(6)
-    let noonTimeZoneISO = String(noonTimeDate + noonTimeTime + noonTimeTimezone)
-    
-    let noonDateFormatter = DateFormatter()
-    noonDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-    let noon = noonDateFormatter.date(from: noonTimeZoneISO)
-    return noon!
-}
-
-/// Returns bool indicating if the app/widget should check for new data in near future.
-fileprivate func needToCheckForNewData(basedOn energyData: EnergyData, withRotationTime: Date) -> Bool {
-    guard let lastItemStart = energyData.prices.last?.startTimestamp else { return true }
-    
-    let now = Date()
-
-    let difference = Calendar.init(identifier: .gregorian).compare(lastItemStart, to: now, toGranularity: .day).rawValue
-    if difference > 0 {
-        return false
-    } else {
-        if now >= noonBerlinTimestamp {
-             return true
-        } else {
-            return false
-        }
-    }
-}
-
-fileprivate func getPriceEntryInOneHour() -> PriceWidgetEntry? {
-    guard let startMinute = Calendar.current.date(bySetting: .minute, value: 0, of: Date()) else { return nil }
-    guard let startHour = Calendar.current.date(bySetting: .second, value: 0, of: startMinute) else { return nil }
-    let nextHour = startHour.addingTimeInterval(3600)
-    
-    let entry = PriceWidgetEntry(date: nextHour)
-    return entry
-}
-
 /// Get the current energy data from the app storage. If this energy data needs to be updated or doesn't exist yet the backend is polled. If no energy data could be found at all a a empty energy data object will be returned.
 fileprivate func getCurrentEnergyData(_ setting: CurrentSetting) -> EnergyData {
     // Energy data with default values
@@ -86,6 +38,47 @@ fileprivate func getCurrentEnergyData(_ setting: CurrentSetting) -> EnergyData {
     return energyData
 }
 
+func getTimeZoneTime(hour: Int, minute: Int, second: Int, fromTimeZone timeZone: String) -> Date {
+    let timeComponentFormatter = NumberFormatter()
+    timeComponentFormatter.maximumIntegerDigits = 2
+    let hourString = timeComponentFormatter.string(from: NSNumber(value: hour)) ?? "00"
+    let minuteString = timeComponentFormatter.string(from: NSNumber(value: minute)) ?? "00"
+    let secondString = timeComponentFormatter.string(from: NSNumber(value: second)) ?? "00"
+    
+    let isoDateFormatter = DateFormatter()
+    isoDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    isoDateFormatter.timeZone = TimeZone(identifier: "Europe/Berlin")
+    let nowBerlinISO = isoDateFormatter.string(from: Date())
+    
+    let noonTimeDate = nowBerlinISO.prefix(11)
+    let noonTimeTime = "\(hourString):\(minuteString):\(secondString)"
+    let noonTimeTimezone = nowBerlinISO.suffix(6)
+    let noonTimeZoneISO = String(noonTimeDate + noonTimeTime + noonTimeTimezone)
+    
+    let noonDateFormatter = DateFormatter()
+    noonDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    let noon = noonDateFormatter.date(from: noonTimeZoneISO)
+    return noon!
+}
+
+/// Returns bool indicating if the app/widget should check for new data in near future.
+fileprivate func needToCheckForNewData(basedOn energyData: EnergyData, withRotationTime rotationTime: Date) -> Bool {
+    guard let lastItemStart = energyData.prices.last?.startTimestamp else { return true }
+    
+    let now = Date()
+
+    let difference = Calendar.init(identifier: .gregorian).compare(lastItemStart, to: now, toGranularity: .day).rawValue
+    if difference > 0 {
+        return false
+    } else {
+        if now >= rotationTime {
+             return true
+        } else {
+            return false
+        }
+    }
+}
+
 func getNewPriceTimeline(
     in context: TimelineProviderContext,
     completion: @escaping (Timeline<PriceWidgetEntry>) -> ()
@@ -94,17 +87,17 @@ func getNewPriceTimeline(
     let persistence = PersistenceManager()
     let setting = CurrentSetting(managedObjectContext: persistence.persistentContainer.viewContext)
     var energyData = getCurrentEnergyData(setting)
-    let noonBerlin = getTodayNoonDate(fromTimeZone: "Europe/Berlin") // aWATTar energy data is based on curren Europe/Berlin time
-    
-    let needToCheckForNewData = needToCheckForNewData(basedOn: energyData, withRotationTime: noonBerlin)
+    let rotationTime = getTimeZoneTime(hour: 12, minute: 0, second: 0, fromTimeZone: "Europe/Berlin") // Set time after which AWattPrice should check for new energy data.
+    let needToCheckForNewData = needToCheckForNewData(basedOn: energyData, withRotationTime: rotationTime)
 
     var entries = [PriceWidgetEntry]()
+    let timeline: Timeline<PriceWidgetEntry>? = nil
     if needToCheckForNewData {
         
     } else {
-        
+        timeline = timelineUntilNextRotationTime(rotationTime)
     }
     
-    completion(timeline)
+    completion(timeline!)
     return
 }

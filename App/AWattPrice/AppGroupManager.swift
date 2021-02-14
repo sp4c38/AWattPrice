bc6835434d8e5fc2aeec8087e54d645ac322a37f
Léon Becker <lb@alien8.de>
2021-02-14 16:28:29 +0100

    AppGroupManager now with groupID at initialization
    Made multiple changes to the AppGroupManager:
    - The groupID must now be parsed at initialization. Approach before was to set the group id after initialization - duh.
    - Added new regionToFileName function which converts a Region object to the appropriate file name stored in the app group.
    - Added new error handling for read and write functions.
    - The getEnergyDataStored function will now not decode the energy data anymore.
    - The AppGroupManagerKey EnvironmentKey was removed.

--- App/AWattPrice/AppGroupManager.swift
+++ App/AWattPrice/AppGroupManager.swift
@@ -14,10 +14,19 @@ enum AppGroups {
 }
 
 class AppGroupManager {
-    var groupID: String? = nil
-    var containerURL: URL? = nil
+    var groupID: String
+    var containerURL: URL
     
-    private var energyStoreFileName = "EnergyData.json"
+    init?(withID groupID: String) {
+        self.groupID = groupID
+        self.containerURL = URL(fileURLWithPath: "")
+        
+        guard let newContainerURL = getContainerURL(groupID) else {
+            logger.error("A group container with the supplied ID \(groupID) doesn't exist.")
+            return nil
+        }
+        containerURL = newContainerURL
+    }
     
     private func getContainerURL(_ containerID: String) -> URL? {
         let newContainerURL = FileManager.default.containerURL(
@@ -27,17 +36,28 @@ class AppGroupManager {
         return containerURL
     }
     
-    public func setGroup(_ newGroupID: String) -> Bool {
-        guard groupID != newGroupID else { return true }
-        guard let newContainerURL = getContainerURL(newGroupID) else { return false }
-        groupID = newGroupID
-        containerURL = newContainerURL
-        return true
+    internal func regionToFileName(_ region: Region) -> String {
+        let rootNamePrefix = "EnergyData_"
+        var regionName = ""
+        let rootNameSuffix = ".json"
         
+        if region == .DE {
+            regionName = "DE"
+        } else if region == .AT {
+            regionName = "AT"
+        }
+        
+        return rootNamePrefix + regionName + rootNameSuffix
     }
     
-    public func writeEnergyDataToGroup(energyData: EnergyData) -> Bool {
-        guard let parentURL = containerURL else { return false }
+    enum ReadWriteError: Error {
+        case readFromFileError
+        case writeEncodingError
+        case writeToFileError
+    }
+    
+    public func writeEnergyDataToGroup(energyData: EnergyData, forRegion region: Region) -> Error? {
+        // Encode
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .secondsSince1970
         var encodedEnergyData: Data?
@@ -45,54 +65,32 @@ class AppGroupManager {
             encodedEnergyData = try encoder.encode(energyData)
         } catch {
             logger.error("Could encode energy data when writing to app group container: \(error.localizedDescription).")
-            return false
+            return ReadWriteError.writeEncodingError
         }
-        let storeURL = parentURL.appendingPathComponent("EnergyData.json")
+        
+        // Write to file
+        let storeURL = containerURL.appendingPathComponent("EnergyData.json")
         do {
             try encodedEnergyData!.write(to: storeURL)
             logger.debug("Wrote energy data to group container.")
         } catch {
             logger.error("Couldn't write energy data to app group container: \(error.localizedDescription).")
-            return false
+            return ReadWriteError.writeToFileError
         }
-        return true
+        return nil
     }
     
-    public func readEnergyData() -> EnergyData? {
-        guard let parentURL = containerURL else {
-            logger.notice("No app group set when trying to read energy data from app group container..")
-            return nil
-        }
-        let storeURL = parentURL.appendingPathComponent("EnergyData.json")
-        var encodedEnergyData = Data()
+    public func getEnergyDataStored(for region: Region) -> (Data?, Error?) {
+        let storeURL = containerURL.appendingPathComponent(regionToFileName(region))
+        
+        var data = Data()
         do {
-            encodedEnergyData = try Data(contentsOf: storeURL)
+            data = try Data(contentsOf: storeURL)
         } catch {
             // Triggered if EnergyData.json doesn't yet exist.
             logger.info("Couldn't read file with energy data from app group container: \(error.localizedDescription).")
-            return nil
+            return (nil, ReadWriteError.readFromFileError)
         }
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .secondsSince1970
-        var decodedEnergyData: EnergyData? = nil
-        do {
-            decodedEnergyData = try decoder.decode(EnergyData.self, from: encodedEnergyData)
-            logger.debug("Read energy data from group container.")
-        } catch {
-            logger.error("Couldn't decode energy data after reading from app group container: \(error.localizedDescription).")
-        }
-        guard let energyData = decodedEnergyData else { return nil }
-        return energyData
-    }
-}
-
-class AppGroupManagerKey: EnvironmentKey {
-    static var defaultValue = AppGroupManager()
-}
-
-extension EnvironmentValues {
-    var appGroupManager: AppGroupManager {
-        get { self[AppGroupManagerKey.self] }
-        set {}
+        return (data, nil)
     }
 }

f1742864db567bb7cbea5b77f405b40c21a67ddf
Léon Becker <lb@alien8.de>
2021-02-15 12:04:19 +0100

    Updated getTimeBySetting function
    - Renamed getTimeZoneTimeBySetting -> getTimeBySetting.
    - Found simpler way to set the components of the current time in the specified time zone to the parsed components.

--- App/AWattPrice/AWattPrice/Extensions/Other/TimeZoneTime.swift
+++ App/AWattPrice/AWattPrice/Extensions/Other/TimeZoneTime.swift
@@ -7,59 +7,37 @@
 
 import Foundation
 
-fileprivate func packNewTimeComponent(comp: String, isLast: Bool) -> String {
-    if isLast {
-        return comp
-    } else {
-        return "\(comp):"
+enum SingleDateComponents {
+    case year, month, day
+    case hour, minute, second
+}
+
+/** Set the components of the current time in the specified time zone to the parsed components.
+ The value of components not included in the parsed list are retained.
+ 
+ Example of functionality:
+    Get current time -> set components which are included in the parsed components list to the specified value ->
+    create Date object from components -> return
+    So, the returned Date object reflects the timestamp at which the components in the specified timezone would match the parsed components.
+ 
+ - returns: Date object representing the certain time with the set components.
+ */
+func getTimeBySetting(
+    _ components: [SingleDateComponents: Int], inTimeZone timeZone: TimeZone)
+-> Date? {
+    let now = Date()
+    var newComponents = Calendar.current.dateComponents(in: timeZone, from: now)
+    
+    for comp in components.keys {
+        let newValue = components[comp]
+        if comp == .second { newComponents.second = newValue }
+        if comp == .minute { newComponents.minute = newValue }
+        if comp == .hour { newComponents.hour = newValue }
+        if comp == .day { newComponents.day = newValue }
+        if comp == .month { newComponents.month = newValue }
+        if comp == .year { newComponents.year = newValue }
     }
-}
-
-fileprivate func addNewTimeComponent(
-    newComp: String?, original: String, isLast: Bool = false
-) -> String {
-    if newComp != nil {
-        return packNewTimeComponent(comp: newComp!, isLast: isLast)
-    } else {
-        return packNewTimeComponent(comp: original, isLast: isLast)
-    }
-}
-
-fileprivate func longISOSetTimeComponents(toHour hour: Int?, minute: Int?, second: Int?, of timeISO: String) -> String {
-    let timeComponentFormatter = NumberFormatter()
-    timeComponentFormatter.minimumIntegerDigits = 2
-    timeComponentFormatter.maximumIntegerDigits = 2
-    let hourString: String? = timeComponentFormatter.string(from: NSNumber(value: hour ?? 0))
-    let minuteString: String? = timeComponentFormatter.string(from: NSNumber(value: minute ?? 0))
-    let secondString: String? = timeComponentFormatter.string(from: NSNumber(value: second ?? 0))
     
-    let dateComp = timeISO.part(inRange: 0..<11)
-    let timeComp = timeISO.part(inRange: 11..<19)
-    let utcOffsetComp = timeISO.part(inRange: 19..<25) // Indicates the timezone of the iso
-    
-    let hourComp = timeComp.part(inRange: 0..<2)
-    let minuteComp = timeComp.part(inRange: 3..<5)
-    let secondComp = timeComp.part(inRange: 6..<8)
-    
-    var newTime = ""
-    newTime += addNewTimeComponent(newComp: hourString, original: hourComp)
-    newTime += addNewTimeComponent(newComp: minuteString, original: minuteComp)
-    newTime += addNewTimeComponent(newComp: secondString, original: secondComp, isLast: true)
-    
-    let newISO = dateComp + newTime + utcOffsetComp
-    return newISO
-}
-
-func getTimeZoneTimeBySetting(hour: Int?, minute: Int?, second: Int?, usingTimeZone: String) -> Date? {
-    let isoFormatter = DateFormatter()
-    isoFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-    isoFormatter.timeZone = TimeZone(identifier: "Europe/Berlin")
-    let nowTimeZoneISO = isoFormatter.string(from: Date())
-    
-    let modifiedISO = longISOSetTimeComponents(
-        toHour: hour, minute: minute, second: second, of: nowTimeZoneISO
-    )
-    let modifiedTimeZoneDate = isoFormatter.date(from: modifiedISO)
-
-    return modifiedTimeZoneDate
+    let newDate = Calendar.current.date(from: newComponents)
+    return newDate
 }

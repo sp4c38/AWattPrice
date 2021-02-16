368f81ea72d21a8581011028dacc01418c171f10
Léon Becker <lb@alien8.de>
2021-02-16 09:46:28 +0100

    Change function to run tasks in queue if a condition is true.
    What:
    - Merge runQueueSyncOrAsync in runInQueueIf func.
    - Removed runAsync parameter & functionality, as it didn't make any sense.
    
    Why: Remove some bad coding.

--- App/AWattPrice/AWattPrice/Extensions/Helpers/QueueHelpers.swift
+++ App/AWattPrice/AWattPrice/Extensions/Helpers/QueueHelpers.swift
@@ -7,41 +7,22 @@
 
 import Foundation
 
-/// Runs the tasks asynchronous if runAsync is true. If not the tasks will be ran synchronous. All tasks are run in in the specified run queue (for example: main queue, the global queue with qos background).
-func runQueueSyncOrAsync(
-    _ runQueue: DispatchQueue,
-    _ runAsync: Bool,
-    deadlineIfAsync: DispatchTime? = nil,
+/// Run task in queue if condition is true. Optionally parse deadline - is only needed if condition is true, as tasks are only then run asynchronous.
+func runInQueueIf(
+    isTrue condition: Bool,
+    in runQueue: DispatchQueue,
+    withDeadline deadline: DispatchTime? = nil,
     tasks: @escaping () -> ()
 ) {
-    if runAsync {
-        if deadlineIfAsync != nil {
-            runQueue.asyncAfter(deadline: deadlineIfAsync!) {
+    if condition {
+        if deadline != nil {
+            runQueue.asyncAfter(deadline: deadline!) {
                 tasks()
             }
         } else {
             runQueue.async {
                 tasks()
             }
-        }
-    } else {
-        runQueue.sync {
-            tasks()
-        }
-    }
-}
-
-/// Will run the tasks (synchronous or asynchronous) in the specified run queue if the condition is true. Therefor the function runQueueSyncOrAsync is used in the background. If the condition is false the tasks will be ran synchronous in the current queue.
-func runInQueueIf(
-    isTrue condition: Bool,
-    in runQueue: DispatchQueue,
-    runAsync: Bool,
-    withDeadlineIfAsync: DispatchTime? = nil,
-    tasks: @escaping () -> ()
-) {
-    if condition {
-        runQueueSyncOrAsync(runQueue, runAsync, deadlineIfAsync: withDeadlineIfAsync) {
-            tasks()
         }
     } else {
         tasks()

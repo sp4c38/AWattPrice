eecc05a3e7dbaf2354a02800ca8a01f0f4b333db
Léon Becker <lb@space8.me>
2021-09-09 22:14:17 +0200

    Added old notification config to new notification config conversion.

--- backend/misc/transfer_database.py
+++ backend/misc/transfer_database.py
@@ -6,39 +6,90 @@ import sys
 from awattprice import configurator
 from awattprice.database import get_awattprice_engine
 from awattprice.database import get_engine
+from awattprice.defaults import Region
+from awattprice.orm import Token
+from awattprice.orm import PriceBelowNotification
 from box import Box
 from sqlalchemy import Column
+from sqlalchemy import Boolean
+from sqlalchemy import Enum
 from sqlalchemy import Integer
 from sqlalchemy import MetaData
 from sqlalchemy import select
 from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import registry as Registry
+from sqlalchemy.types import TypeDecorator
 
 TRANSFER_DATABASE_SERVICE_NAME = "transfer_database"
 
-async def get_old_notification_config(old_database: AsyncEngine) -> list:
-	metadata = MetaData()
-	notification_config_table = Table("token_storage", metadata,
-		Column("token", String, primary_key=True),
-		Column("region_identifier", Integer),
-		Column("vat_selection", Integer),
-		Column("configuration", String)
-	)
-	request = select(notification_config_table)
-	async with old_database.connect() as connection:
-		raw_results = await connection.execute(request)
+metadata = MetaData()
+registry = Registry(metadata)
+Base = registry.generate_base()
 
-	notification_configs = [Box(dict(x)) for x in raw_results.fetchall()]
-	for notification_config in notification_configs:
+class JSONField(TypeDecorator):
+	impl = String
+
+	def process_result_value(self, value, dialect):
 		try:
-			notification_config.configuration = Box(json.loads(notification_config.configuration))
+			return Box(json.loads(value))
 		except json.JSONDecodeError as exc:
-			logger.error(f"Old notification config configuration is no valid json: {notification_config.configuration} - {exc}.")
+			logger.error(f"Couldn't decode json field {value}: {exc}.")
+			return None
+
+class RegionIdentifierField(TypeDecorator):
+	impl = Integer
+
+	def process_result_value(self, value, dialect):
+		if value == 0:
+			return Region.DE
+		elif value == 1:
+			return Region.AT
+		else:
+			return None
+
+
+class OldTokenStorage(Base):
+	__tablename__ = "token_storage"
+
+	token = Column(String, primary_key=True)
+	region_identifier = Column(RegionIdentifierField)
+	vat_selection = Column(Boolean)
+	configuration = Column(JSONField)
+
+
+async def get_old_notification_configs(old_database: AsyncEngine) -> list:
+	async with AsyncSession(old_database) as session:
+		raw_results = await session.execute(select(OldTokenStorage))
+
+	notification_configs = raw_results.scalars().all()
+	for notification_config in notification_configs:
+		if notification_config is None:
+			logger.error("Couldn't get notification configuration.")
 			sys.exit(1)
 
 	return notification_configs
 
+def convert_to_entries(old_notification_configs: list[Box]) -> Box:
+	"""Convert the entries of the old database to table row entries for the new database."""
+	new_entries = Box(token=[], price_below_notification=[])
+	for notification_config in old_notification_configs:
+		new_token = Token(
+			token=notification_config.token,
+			region=notification_config.region_identifier,
+			tax=notification_config.vat_selection
+		)
+		price_below_config = notification_config.configuration.config.price_below_value_notification
+		new_price_below_notification = PriceBelowNotification(
+			active=price_below_config.active,
+			below_value=price_below_config.below_value
+		)
+		new_entries.token.append(new_token)
+		new_entries.price_below_notification.append(new_price_below_notification)
+
+	return new_entries
 
 async def main():
 	config = configurator.get_config()
@@ -50,8 +101,9 @@ async def main():
 	new_database = get_awattprice_engine(config, async_=True)
 	old_database = get_engine(config.paths.old_database, async_=True)
 
-	old_notification_config = await get_old_notification_config(old_database)
-	print(old_notification_config)
+	old_notification_configs = await get_old_notification_configs(old_database)
+	print(convert_to_entries(old_notification_configs))
+
 
 
 if __name__ == '__main__':

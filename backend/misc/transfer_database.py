e07ec267f6e145ed163807a532be1bbddc7b84ce
Léon Becker <lb@space8.me>
2021-09-12 16:56:52 +0200

    Added database entry transferal.

--- backend/misc/transfer_database.py
+++ backend/misc/transfer_database.py
@@ -3,6 +3,8 @@ import asyncio
 import json
 import sys
 
+from collections import namedtuple
+
 from awattprice import configurator
 from awattprice.database import get_awattprice_engine
 from awattprice.database import get_engine
@@ -10,6 +12,7 @@ from awattprice.defaults import Region
 from awattprice.orm import Token
 from awattprice.orm import PriceBelowNotification
 from box import Box
+from loguru import logger
 from sqlalchemy import Column
 from sqlalchemy import Boolean
 from sqlalchemy import Enum
@@ -28,6 +31,8 @@ TRANSFER_DATABASE_SERVICE_NAME = "transfer_database"
 metadata = MetaData()
 registry = Registry(metadata)
 Base = registry.generate_base()
+
+NewEntryPair = namedtuple("NewEntryPair", ["token", "price_below_notification"])
 
 class JSONField(TypeDecorator):
 	impl = String
@@ -60,22 +65,17 @@ class OldTokenStorage(Base):
 	configuration = Column(JSONField)
 
 
-async def get_old_notification_configs(old_database: AsyncEngine) -> list:
+async def get_old_entries(old_database: AsyncEngine) -> list:
 	async with AsyncSession(old_database) as session:
 		raw_results = await session.execute(select(OldTokenStorage))
 
-	notification_configs = raw_results.scalars().all()
-	for notification_config in notification_configs:
-		if notification_config is None:
-			logger.error("Couldn't get notification configuration.")
-			sys.exit(1)
+	entries = raw_results.scalars().all()
+	return entries
 
-	return notification_configs
-
-def convert_to_entries(old_notification_configs: list[Box]) -> Box:
+def convert_to_new_entries(old_entries: list[Box]) -> list:
 	"""Convert the entries of the old database to table row entries for the new database."""
-	new_entries = Box(token=[], price_below_notification=[])
-	for notification_config in old_notification_configs:
+	new_entries = []
+	for notification_config in old_entries:
 		new_token = Token(
 			token=notification_config.token,
 			region=notification_config.region_identifier,
@@ -86,10 +86,38 @@ def convert_to_entries(old_notification_configs: list[Box]) -> Box:
 			active=price_below_config.active,
 			below_value=price_below_config.below_value
 		)
-		new_entries.token.append(new_token)
-		new_entries.price_below_notification.append(new_price_below_notification)
+		new_entry = NewEntryPair(token=new_token, price_below_notification=new_price_below_notification)
+		new_entries.append(new_entry)
 
 	return new_entries
+
+async def transfer_entries(old_entries: list[OldTokenStorage], old_database: AsyncEngine, converted_entries: list[NewEntryPair], new_database: AsyncEngine):
+	"""Remove entries from the old database and add the converted entries to the new database.
+
+	:param old_entries and new_entries: All old entries must be contained in new_entries.
+	"""
+	async with AsyncSession(new_database) as new_db_session, AsyncSession(old_database) as old_db_session:
+		for old_entry in old_entries:
+			await old_db_session.delete(old_entry)
+		await old_db_session.flush()
+
+		token_entries = [e.token for e in converted_entries]
+		new_db_session.add_all(token_entries)
+
+		await new_db_session.flush()
+		for entry in converted_entries:
+			entry.price_below_notification.token_id = entry.token.token_id
+
+		price_below_notification_entries = [e.price_below_notification for e in converted_entries]
+		new_db_session.add_all(price_below_notification_entries)
+		new_db_session.flush()
+
+		await new_db_session.commit()
+		await old_db_session.commit()
+
+		logger.info(f"Transfered {len(token_entries)} token entry/entries " \
+					f"and {len(price_below_notification_entries)} price below notification entries.")
+
 
 async def main():
 	config = configurator.get_config()
@@ -101,9 +129,14 @@ async def main():
 	new_database = get_awattprice_engine(config, async_=True)
 	old_database = get_engine(config.paths.old_database, async_=True)
 
-	old_notification_configs = await get_old_notification_configs(old_database)
-	print(convert_to_entries(old_notification_configs))
+	old_entries = await get_old_entries(old_database)
+	if len(old_entries) == 0:
+		logger.info("No entries from the old database to transfer.")
+		sys.exit(0)
 
+	converted_entries = convert_to_new_entries(old_entries)
+
+	await transfer_entries(old_entries, old_database, converted_entries, new_database)
 
 
 if __name__ == '__main__':

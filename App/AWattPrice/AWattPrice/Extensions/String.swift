38952965c4d99da5f608d6f604daa4d998914477
Léon Becker <lb@space8.me>
2025-03-17 17:00:04 +0100

    Removed a bunch of unnecessary stuff

--- App/AWattPrice/AWattPrice/Extensions/String.swift
+++ App/AWattPrice/AWattPrice/Extensions/String.swift
@@ -53,50 +53,9 @@ extension String {
 }
 
 extension String {
-    func removeOutOfString(atIndex index: Int) -> String {
-        var before = ""
-        if index - 1 >= 0 {
-            before = String(self[...self.index(startIndex, offsetBy: index - 1)])
-        }
-        let after = String(self[self.index(startIndex, offsetBy: index + 1)...])
-        let newString = before + after
-        return newString
-    }
-}
-
-extension String {
-    func addAtIndex(atIndex index: Int, add addString: String) -> String {
-        var before = ""
-        if index - 1 >= 0 {
-            before = String(self[...self.index(startIndex, offsetBy: index - 1)])
-        }
-        let after = String(self[self.index(startIndex, offsetBy: index)...])
-        let newString = before + addString + after
-        return newString
-    }
-}
-
-extension String {
     /// Returns the localized string of a string.
     /// If you wish to format a localized string use String(format: String.localized(), value)
     func localized() -> String {
         NSLocalizedString(self, comment: "")
-    }
-}
-
-extension String {
-    func part(inRange range: Range<Int>) -> String {
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(startIndex, offsetBy: range.upperBound)
-        
-        let range = start..<end
-        return String(self[range])
-    }
-}
-    
-extension String {
-    var completeNSRange: NSRange {
-        let range = NSRange(location: 0, length: self.utf16.count)
-        return range
     }
 }
